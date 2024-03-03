65edd00e2d59052b07ddd7fc62bab7d92cde6a92
xiaoguadai <zack40091647@gmail.com>
2024-03-03 16:11:07 +0800

    q_free reference: nosba0957

--- queue.c
+++ queue.c
@@ -25,7 +25,15 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head) 
+{
+    if (!head) 
+        return;
+    element_t *entry, *safe;
+    list_for_each_entry_safe (entry, safe, head, list)
+        q_release_element(entry);
+    free(head); 
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
