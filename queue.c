d656ae4765f4960cdb589b03d5f8bfd0083f5b16
xiaoguadai <zack40091647@gmail.com>
2024-03-04 21:27:28 +0800

    q_size create

--- queue.c
+++ queue.c
@@ -62,7 +62,13 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+    size_t len = 0;
+    struct list_head *iter;
+    list_for_each (iter, head)
+        len++;
+    return len;
 }
 
 /* Delete the middle node in queue */
